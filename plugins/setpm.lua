b7594e981ea380abcdc62d397a950fb3ca578098
Master-DiMoN <MasTerDiMoN2@gmail.com>
2016-08-08 20:32:28 +0430

    Update setpm.lua

--- plugins/setpm.lua
+++ plugins/setpm.lua
@@ -1,13 +1,13 @@
 function run (msg,matches)
   if matches[1] == "set" and matches[2] == "sudo" and is_sudo(msg) then
     local waiting = redis:set('sudo:','waiting:'..msg.from.id)
-    return 'متن جدید را ارسال کنید🙁✋'
+    return 'متن جدید را ارسال کنید✔'
     else
     local output = redis:get('sudo:')
     if output == 'waiting:'..msg.from.id then
    if redis:set('sudo:','waiting:'..msg.from.id) and is_sudo(msg) then
       local setted = redis:set('sudo:',msg.text)
-        return '🎈متن جدید ثبت شد'
+        return '✅متن جدید ثبت شد'
       end 
 end 
 
@@ -25,6 +25,3 @@ patterns = {
    },
 run = run,
 }
-
---@SUDO_TM
---@OPENPLUGINS
