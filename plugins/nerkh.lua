7ffbb986ce87dbea069209fffa9f19ff4eeedd61
Master-DiMoN <MasTerDiMoN2@gmail.com>
2016-08-09 00:33:53 +0430

    Update nerkh.lua

--- plugins/nerkh.lua
+++ plugins/nerkh.lua
@@ -1,10 +1,7 @@
 do
 
 function run(msg, matches)
-  return 'نرخ گروه ها '.. [[
-msg.from.print_name.."\n➖➖➖➖➖➖➖➖➖➖➖\n.......\n nmsg.from.first_name\n➖➖➖➖➖➖➖➖➖➖➖\n.....\n msg.from.last_name\n➖➖➖➖➖➖➖➖➖➖➖\n....
-\n msg.from.username\n➖➖➖➖➖➖➖➖➖➖➖\n.....\n msg.from.id\n➖➖➖➖➖➖➖➖➖➖➖\n.....\n
-msg.to.print_name\n ➖➖➖➖➖➖➖➖➖➖➖\n.....\n msg.to.id\n➖➖➖➖➖➖➖➖➖➖➖\n........\n mgs.text\n ➖➖➖➖➖➖➖➖➖➖➖\n.....\n msg.id
+  return ':)'
  ]]
 end
 
